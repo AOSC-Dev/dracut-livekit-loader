4a5e3a76d7c1e0934af04d94a1736e4058e6b460
Cyan <cyan@cyano.uk>
2024-06-14 10:04:15 +0800

    module: add module definition

---
+++ 90aosc-livekit-loader/module-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+check() {
+	# host-only images are meant to be generic.
+	[[ $hostonly ]] && return 1
+	return 255
+}
+
+depends() {
+	echo dm rootfs-block img-lib overlayfs
+	return 0
+}
+
+install() {
+	inst_multiple dmsetup umount mount dd losetup find mkdir rmdir
+	inst_hook cmdline 30 "$moddir"/parse-aosc-cmdline.sh
+	inst_hook pre-udev 30 "$moddir"/aosc-livekit-gen-rules.sh
+	inst_script "$moddir"/livekit-mount-layers.sh /sbin/livekit-mount-layers
+	# Do we need initqueue?
+}
+
+installkernel() {
+	instmods squashfs loop iso9660 overlayfs
+}
+
