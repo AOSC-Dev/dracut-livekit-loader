73fc9199fae00887c4e4ae9f0a850fb12c2d0da7
Cyan <cyan@cyano.uk>
2024-09-11 16:38:06 +0800

    livekit-mount-layers: rename to livekit-loader

--- 90aosc-livekit-loader/module-setup.sh
+++ 90aosc-livekit-loader/module-setup.sh
@@ -14,7 +14,7 @@ install() {
 	inst_multiple dmsetup umount mount dd losetup find mkdir rmdir tee less realpath bc head awk grep cut nproc
 	inst_hook cmdline 30 "$moddir"/parse-aosc-cmdline.sh
 	inst_hook pre-udev 30 "$moddir"/aosc-livekit-gen-rules.sh
-	inst_script "$moddir"/livekit-mount-layers.sh /sbin/livekit-mount-layers
+	inst_script "$moddir"/livekit-loader.sh /sbin/livekit-loader
 	# Do we need initqueue?
 }
 
